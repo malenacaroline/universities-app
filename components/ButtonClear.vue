970a8fcdce1a9aa5294fb9c894679834806556b0
Malena Aguiar <malenaaguiar@Malenas-Mac-Mini.local>
2024-02-03 09:46:01 -0700

    add more ui adjustments

--- components/ButtonClear.vue
+++ components/ButtonClear.vue
@@ -1,9 +1,9 @@
 <template>
   <button
-    class="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-700 rounded-lg group bg-gradient-to-br from-gray-700 to-gray-500 group-hover:from-gray-700 group-hover:to-gray-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 shadow-lg dark:shadow-teal-800/80"
+    class="relative flex justify-center items-center text-center p-0.5 overflow-hidden text-sm font-medium text-gray-700 rounded-lg group bg-gradient-to-br from-gray-700 to-gray-500 group-hover:from-gray-700 group-hover:to-gray-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 shadow-lg dark:shadow-teal-800/80 w-full md:w-auto"
   >
     <span
-      class="relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-gray-800 flex justify-items-center hover:text-white"
+      class="relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-gray-800 flex justify-center hover:text-white w-full md:w-auto"
       >Clear Filters
     </span>
   </button>
--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,13 @@ const { clearFilters, searchCountry, searchName } = useUniversitiesData();
 </script>
 <template>
   <div class="max-w-screen-xl py-6 px-4 mx-auto">
-    <form
+    <div
       class="flex flex-col md:flex-row flex-wrap space-y-4 md:space-x-4 sm:space-y-0 items-center justify-between pb-6"
     >
       <InputSearch v-model="searchName" />
       <Dropdown v-model="searchCountry" />
       <ButtonClear @click="clearFilters" />
-    </form>
+    </div>
     <Table />
   </div>
 </template>
